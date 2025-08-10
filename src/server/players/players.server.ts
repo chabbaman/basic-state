8a9b55f159ae6a5a4dfa04035ddedd654daf3c9f
chabbaman <chabbaman10@gmail.com>
2025-08-09 20:12:07 -0700

    Remove unused player cash update logic from player server management.

--- src/server/players/players.server.ts
+++ src/server/players/players.server.ts
@@ -3,7 +3,6 @@ import { Players } from "@rbxts/services";
 import { t } from "@rbxts/t";
 import { type PlayerData } from "shared/producers/players_producer";
 import { RootProducer } from "../store/root_producer";
-import { produce } from "@rbxts/immut";
 
 export const store = createPlayerStore<PlayerData>({
 	name: "TEST-2",
@@ -30,18 +29,6 @@ Players.PlayerAdded.Connect((player) => {
 		userId: player.UserId,
 	});
 	RootProducer.setPlayerData(player.UserId, data);
-
-	task.spawn(() => {
-		do {
-			task.wait(1);
-
-			store.updateImmutableAsync(player, (data) => {
-				return produce(data, (draft) => {
-					draft.cash += 1;
-				});
-			});
-		} while (true);
-	});
 });
 
 Players.PlayerRemoving.Connect((player) => {
